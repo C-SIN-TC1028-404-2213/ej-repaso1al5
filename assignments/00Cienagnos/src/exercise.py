2fa5b278e8cd7d1395a807a705afea58ed949d35
Ricardo Quintero <55771796+rquinteroTecSin@users.noreply.github.com>
2022-09-16 07:59:42 -0600

    Initial commit

---
+++ assignments/00Cienagnos/src/exercise.py
@@ -0,0 +1,11 @@
+def main():
+    #escribe tu código abajo de esta línea
+    edad=int(input(">>>"))
+    agno=int(input(">>>"))
+
+    agno100=(agno-edad)+100
+
+    print(agno100)
+
+if __name__=='__main__':
+    main()
